e829497c5c0c9c36ab66fb08a8394ee264646344
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-15 13:16:04 -0500

    added error handling.

--- server/routes/shopify.js
+++ server/routes/shopify.js
@@ -7,15 +7,18 @@ const SHOPIFY_STORE_URL = process.env.SHOPIFY_STORE_URL;
 
 const fetchData = async () => {
   const url = `https://${SHOPIFY_STORE_URL}/admin/api/2023-10/products.json`;
-
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "X-Shopify-Access-Token": SHOPIFY_API_ACCESS_TOKEN,
-    },
-  });
-  const data = await response.json()
-  console.log(data);
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "X-Shopify-Access-Token": SHOPIFY_API_ACCESS_TOKEN,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("trouble fetching data", error);
+  }
 };
 
 fetchData();
