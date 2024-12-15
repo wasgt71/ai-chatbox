815e9cdb778f8d517f059e0118d9895310b020a1
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-15 13:13:10 -0500

    created function that fetches product data.

--- server/routes/shopify.js
+++ server/routes/shopify.js
@@ -1,8 +1,23 @@
 const { Router } = require("express");
 const shopifyRouter = Router();
+require("dotenv").config();
 
+const SHOPIFY_API_ACCESS_TOKEN = process.env.SHOPIFY_API_ACCESS_TOKEN;
+const SHOPIFY_STORE_URL = process.env.SHOPIFY_STORE_URL;
 
+const fetchData = async () => {
+  const url = `https://${SHOPIFY_STORE_URL}/admin/api/2023-10/products.json`;
 
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "X-Shopify-Access-Token": SHOPIFY_API_ACCESS_TOKEN,
+    },
+  });
+  const data = await response.json()
+  console.log(data);
+};
 
+fetchData();
 
 module.exports = shopifyRouter;
