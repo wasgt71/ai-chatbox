2f2a4b1c6d42bbf5c57b67a7861bf1e2e11d4329
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-19 16:13:08 -0500

    added handledata function. retreives shopify store data from server.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-import Message from './components/Message';
-import UserInput from './components/UserInput';
+import Message from "./components/Message";
+import UserInput from "./components/UserInput";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
 
   const handleUserMessage = async (message) => {
-    const newMessages = [...messages, { text: message, sender: 'user' }];
+    const newMessages = [...messages, { text: message, sender: "user" }];
     setMessages(newMessages);
 
     setIsLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:3000/chat', { message });
+      const response = await axios.post("http://localhost:3000/chat", {
+        message,
+        data,
+      });
       const aiMessage = response.data;
       console.log(response);
-      setMessages([...newMessages, { text: aiMessage, sender: 'Nuusero-ai' }]);
+      setMessages([...newMessages, { text: aiMessage, sender: "Nuusero-ai" }]);
     } catch (error) {
-      console.error('Error fetching response from OpenAI:', error);
+      console.error("Error fetching response from OpenAI:", error);
     } finally {
       setIsLoading(false);
     }
-    
   };
+
+  const handleData = async () => {
+    const response = await axios.post("http://localhost:3000/shopify", {});
+    const products = response.data.products.map((product) => product.title);
+    setData(products);
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
 
   return (
     <div className="chat-container">
@@ -42,4 +55,3 @@ const App = () => {
 };
 
 export default App;
-
