2479612e1b49c76e2df329fa1550d2ce320a9a9c
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-23 21:29:57 -0500

    added general shop data.

--- server/routes/shopify.js
+++ server/routes/shopify.js
@@ -5,22 +5,35 @@ require("dotenv").config();
 const SHOPIFY_API_ACCESS_TOKEN = process.env.SHOPIFY_API_ACCESS_TOKEN;
 const SHOPIFY_STORE_URL = process.env.SHOPIFY_STORE_URL;
 
-const fetchData = async () => {
-  const url = `https://${SHOPIFY_STORE_URL}/admin/api/2023-10/products.json`;
+shopifyRouter.post("/", async (req, res) => {
+  //const fetchData = async () => {
+  const productUrl = `https://${SHOPIFY_STORE_URL}/admin/api/2023-10/products.json`;
+  const homeUrl = `https://${SHOPIFY_STORE_URL}/admin/api/2023-10/shop.json`;
+  const policiesUrl = `https://${SHOPIFY_STORE_URL}/admin/api/2023-10/policies.json`;
+
   try {
-    const response = await fetch(url, {
+    const response = await fetch(productUrl, {
       method: "GET",
       headers: {
         "X-Shopify-Access-Token": SHOPIFY_API_ACCESS_TOKEN,
       },
     });
     const data = await response.json();
-    console.log(data);
+    console.log("hello");
+
+    const homeData = await fetch(homeUrl, {
+      method: "GET",
+      headers: {
+        "X-Shopify-Access-Token": SHOPIFY_API_ACCESS_TOKEN,
+      },
+    });
+    const home = await homeData.json();
+
+    res.json({ home, data });
   } catch (error) {
     console.error("trouble fetching data", error);
+    //};
   }
-};
-
-fetchData();
+});
 
 module.exports = shopifyRouter;
